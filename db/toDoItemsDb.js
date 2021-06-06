fe0abd547b06bc408fbe397dbade7f8c0c7d1677
Leah Greenwald <leahgreenwald123@gmail.com>
2021-06-06 13:44:25 -0400

    commit

--- db/toDoItemsDb.js
+++ db/toDoItemsDb.js
@@ -12,9 +12,31 @@ const sqlConfig = {
 
 const getToDoItems = async () => {
     await sql.connect(sqlConfig);
-    const { recordset } = await sql.query('SELECT * FROM ToDoItems');
+    const { recordset } = await sql.query('SELECT tdi.*, c.Name as CategoryName FROM Categories c JOIN ToDoItems tdi ON c.Id = tdi.CategoryId WHERE tdi.CompletedDate IS NULL');
     return recordset;
 }
+
+const getCategories = async () => {
+    await sql.connect(sqlConfig);
+    const { recordset } = await sql.query('SELECT * FROM Categories');
+    return recordset;
+}
+
+const addToDoItem = async ({ title, dueDate, categoryid }) => {
+    await sql.connect(sqlConfig);
+    await sql.query`INSERT INTO ToDoItems (Title, DueDate, CategoryId)
+    VALUES (${title}, ${dueDate}, ${categoryid})`;
+}
+
+const markAsCompleted = async ({ categoryId }) => {
+    await sql.connect(sqlConfig);
+    await sql.query (`UPDATE ToDoItems SET CompletedDate = getdate() WHERE Id = ${categoryId}`);
+}
+
+
 module.exports = {
-    getToDoItems
+    getToDoItems,
+    getCategories,
+    addToDoItem,
+    markAsCompleted
 }
